5973e65934f236a0104cd3d523a0863546be07ca
Vetorart <vetorartdesign@gmail.com>
2020-04-14 13:49:02 -0300

    corrigido para retirada da extensão em sites sem URL amigável.

--- menuativo.js
+++ menuativo.js
@@ -8,16 +8,23 @@
 
 // Crio a variável que irá informar com qual atributo que irei trabalhar
 var atributo = "name";
+// Informo a extensão final. Caso utilize URL amigável, favor retirar este trecho.
+var extension = "."+"html";
+var count_extension = extension.length;
+
 // Crio a variável que irá informar qual o valor do atributo que irei trabalhar
 var atributoValor = "menu";
 // Crio a variável com o nome da classe que será adicionada quando o link ficar ativo
 var classe = "mactive";
 // Resgato a URL atual do navegador
 var url = window.location.href;
+
 // Divido a url
 var split = url.split('/');
 // No meu caso, estou pegando o último parâmetro da URL
 var menu = split[split.length-1];
+// Retira a extensão da url. Caso utilize URL amigável, favor retirar este trecho.
+menu = menu.substring(menu.lenght,count_extension);
 
 // Rodo o menu em busca dos links com o atributo e valor instanciado acima
 var itensMenu = document.querySelectorAll('a['+atributo+'='+atributoValor+']');
